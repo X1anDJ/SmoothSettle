72951c5a9ffecc410440de2fe345f69f415f6217
Dajun <dxian780@gmail.com>
2024-09-22 00:20:44 -0700

    Initial Commit

---
+++ SmoothSettle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmoothSettle
+//
+//  Created by Dajun Xian on 2024/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
